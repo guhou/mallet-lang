52441f978fb04e6b11b8908bf3053a720317bc76
Angus Houston <angus.houston@lexer.com.au>
2020-05-05 22:41:01 +1000

    Specs should only show failed output

--- test/Main.hs
+++ test/Main.hs
@@ -1,1 +1,12 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main
+    ( main
+    )
+where
+
+import qualified MalletTest
+import           Test.Hspec.Formatters
+import           Test.Hspec.Runner
+
+main :: IO ()
+main = hspecWith config MalletTest.spec
+    where config = defaultConfig { configFormatter = Just failed_examples }
